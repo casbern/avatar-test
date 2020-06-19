92294b6cc8a8ee0df626990f1fe874e11d814402
Cássia <cascrisbern@gmail.com>
2020-06-19 13:58:50 -0300

    ajustes

--- cypress/integration/first.spec.js
+++ cypress/integration/first.spec.js
@@ -22,7 +22,7 @@ describe('Preenchimento de período errado', () => {
   })
 
   
-  it('1. Checa se mostra popup para uma data inicial vazia', () => {
+ it('1. Checa se mostra popup para uma data inicial vazia', () => {
     cy.get('#ContentPlaceHolder1_ConPeriodo_txbDataInicial').type(' ')
     cy.get('#ContentPlaceHolder1_ConPeriodo_txbDataFinal').type('31/01/2003')
     cy.get('#ContentPlaceHolder1_btnConsultar').click()
@@ -192,6 +192,7 @@ describe('Preenchimento de período errado', () => {
   it('3g. Checa o preenchimento de campos alterados + atendimento + faturamento', () => {
     cy.get('#ContentPlaceHolder1_ConPeriodo_txbDataInicial').type('01/01/2003')
     cy.get('#ContentPlaceHolder1_ConPeriodo_txbDataFinal').type('10/01/2003').tab()
+
     cy.get('#ContentPlaceHolder1_ConConvenioMulti_ASPxGridLookup_Convenios_B-1')
       .click()
     cy.get('#ContentPlaceHolder1_ConConvenioMulti_ASPxGridLookup_Convenios_DDD_gv_DXFREditorcol1 > tbody > tr > .dxic')
@@ -201,17 +202,15 @@ describe('Preenchimento de período errado', () => {
         expect($p).to.contain('45')
       })
     cy.get('#ContentPlaceHolder1_ConConvenioMulti_ASPxGridLookup_Convenios_DDD_gv_DXSelBtn0_D')
-      .click()
+    .click()
     cy.focused().tab()
 
-    
     cy.get('#ContentPlaceHolder1_ConConvenioMulti_ASPxGridLookup_Convenios_B-1')
-    .click()
-    cy.wait(400)
-    cy.get('#ContentPlaceHolder1_ConConvenioMulti_ASPxGridLookup_Convenios_DDD_PWC-1')
-      .should('be.visible')
-    cy.get('#ContentPlaceHolder1_ConConvenioMulti_ASPxGridLookup_Convenios_DDD_gv_DXFREditorcol1 > tbody > tr > .dxic')
-      .type('20')
+      .click()
+    cy.get('#ContentPlaceHolder1_ConConvenioMulti_ASPxGridLookup_Convenios_DDD_gv_DXFREditorcol1_I')
+      .should('have.value', '')
+    cy.get('#ContentPlaceHolder1_ConConvenioMulti_ASPxGridLookup_Convenios_DDD_gv_DXFREditorcol1_I')
+      .type('20')  
     cy.get('#ContentPlaceHolder1_ConConvenioMulti_ASPxGridLookup_Convenios_DDD_gv_DXDataRow0 > :nth-child(2)')
       .should(($p) => {
         expect($p).to.contain('20')
